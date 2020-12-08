0564ebe332f9c64b7fc280fa913c0118cf765be9
Dennis Mbithi <denmbithi@gmail.com>
2020-12-08 23:03:35 +0300

    Added list and detailed views

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -17,11 +18,20 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     body = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='blog_posts'
+    )
     publish = models.DateTimeField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
-    updated_at =models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default='draft'
+    )
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ('-publish',)
@@ -29,5 +39,13 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-    objects = models.Manager()
-    published = PublishedManager()
+    def get_absolute_url(self):
+        return reverse(
+            'blog:post_detail',
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug
+            ]
+        )
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
 
-# Create your views here.
+
+def home(request):
+    posts = Post.published.all()
+    template = 'blog/post/list.html'
+    context = {
+        'posts': posts,
+    }
+
+    return render(request, template, context)
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             slug=post,
+                             status='published',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day
+                             )
+    print(post)
+    template = 'blog/post/detail.html'
+    context = {
+        'post': post,
+    }
+
+    return render(request, template, context)
